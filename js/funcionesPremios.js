3a176923efeada0364763d60d87143f82c18f24f
Unknown <sarai.gil.ramos12@gmail.com>
2018-06-10 11:59:25 -0500

    Funcionalidad Premios
    Yastaaaaaaaaaaaaaaaa<3

--- js/funcionesPremios.js
+++ js/funcionesPremios.js
@@ -1,3 +1,5 @@
+var ptsSumasFinal;
+var ptsNaipesFinal;
 $(document).ready(function(){
   user = JSON.parse(localStorage.getItem('usuario'))
   nombre = document.getElementById("user");
@@ -17,64 +19,126 @@ $(document).ready(function(){
   premioN3 = document.getElementById("NPremio3");
   premioN4 = document.getElementById("NPremio4");
 
+  if (sumas < 50) {
+    ptsSumasFinal = 1;
+  } else if (sumas >= 50 && sumas < 80) {
+    ptsSumasFinal = 2;
+  } else if (sumas >= 80 && sumas < 110) {
+    ptsSumasFinal = 3;
+  } else if (sumas >= 110 && sumas < 130) {
+    ptsSumasFinal = 4;
+  } else if (sumas >= 130 && sumas < 150) {
+    ptsSumasFinal = 5;
+  }
+
+  if (naipes < 50) {
+    ptsNaipesFinal = 1;
+  } else if (naipes >= 50 && naipes < 80) {
+    ptsNaipesFinal = 2;
+  } else if (naipes >= 80 && naipes < 110) {
+    ptsNaipesFinal = 3;
+  } else if (naipes >= 110 && naipes < 130) {
+    ptsNaipesFinal = 4;
+  } else if (naipes >= 130 && naipes < 150) {
+    ptsNaipesFinal = 5;
+  }
+
+
   //Sumas
-  if (sumas < 50 || naipes < 50) {
-    premioS1.style.display = "none";
-    premioS2.style.display = "none";
-    premioS3.style.display = "none";
-    premioS4.style.display = "none";
-    premioN1.style.display = "none";
-    premioN2.style.display = "none";
-    premioN3.style.display = "none";
-    premioN4.style.display = "none";
+  switch (ptsSumasFinal) {
+ //----------NO PREMIOS PUNTOS < 50---------
+    case 1:
+	    premioS1.style.display = "none";
+   	  premioS2.style.display = "none";
+    	premioS3.style.display = "none";
+    	premioS4.style.display = "none";
+      break;
+//---------- PREMIO 1 - PUNTOS DE 50 A 79 -------
+    case 2:
+    	sinPremiosS.style.display = "none";
+    	premioS1.style.display = "block";
+   	  premioS2.style.display = "none";
+    	premioS3.style.display = "none";
+    	premioS4.style.display = "none";
 
-  } else if ((sumas >=50 && sumas < 80)||(naipes >=50 && naipes < 80)) {
-    sinPremiosS.style.display = "none";
-    sinPremiosN.style.display = "none";
-    premioS1.style.display = "block";
-    premioS2.style.display = "none";
-    premioS3.style.display = "none";
-    premioS4.style.display = "none";
-    premioN1.style.display = "block";
-    premioN2.style.display = "none";
-    premioN3.style.display = "none";
-    premioN4.style.display = "none";
+      break;
+//---------- PREMIO 2 - PUNTOS DE 80 A 109 -------
+    case 3:
+    	sinPremiosS.style.display = "none";
+    	premioS1.style.display = "block";
+     	premioS2.style.display = "block";
+    	premioS3.style.display = "none";
+    	premioS4.style.display = "none";
 
-  } else if ((sumas >=80 && sumas < 110)||(naipes >=80 && naipes < 110)) {
-    sinPremiosS.style.display = "none";
-    sinPremiosN.style.display = "none";
-    premioS1.style.display = "block";
-    premioS2.style.display = "block";
-    premioS3.style.display = "none";
-    premioS4.style.display = "none";
-    premioN1.style.display = "block";
-    premioN2.style.display = "block";
-    premioN3.style.display = "none";
-    premioN4.style.display = "none";
+      break;
+//---------- PREMIO 3 - PUNTOS DE 110 A 129 -------
+    case 4:
+    	sinPremiosS.style.display = "none";
+    	premioS1.style.display = "block";
+     	premioS2.style.display = "block";
+    	premioS3.style.display = "block";
+    	premioS4.style.display = "none";
 
-  } else if ((sumas >=110 && sumas < 150)||(naipes >=110 && naipes < 150)) {
-    sinPremiosS.style.display = "none";
-    sinPremiosN.style.display = "none";
-    premioS1.style.display = "block";
-    premioS2.style.display = "block";
-    premioS3.style.display = "block";
-    premioS4.style.display = "none";
-    premioN1.style.display = "block";
-    premioN2.style.display = "block";
-    premioN3.style.display = "block";
-    premioN4.style.display = "none";
-
-  } else if (sumas >=150 || naipes >=150) {
-    sinPremiosS.style.display = "none";
-    sinPremiosN.style.display = "none";
-    premioS1.style.display = "block";
-    premioS2.style.display = "block";
-    premioS3.style.display = "block";
-    premioS4.style.display = "block";
-    premioN1.style.display = "block";
-    premioN2.style.display = "block";
-    premioN3.style.display = "block";
-    premioN4.style.display = "block";
+      break;
+//---------- PREMIO 4 - PUNTOS DE 130 A 150 -------
+    case 5:
+    	sinPremiosS.style.display = "none";
+    	premioS1.style.display = "block";
+     	premioS2.style.display = "block";
+    	premioS3.style.display = "block";
+    	premioS4.style.display = "block";
+      break;
+    default:
 
   }
+
+  //Naipes
+  switch (ptsNaipesFinal) {
+ //----------NO PREMIOS PUNTOS < 50---------
+    case 1:
+      premioN1.style.display = "none";
+      premioN2.style.display = "none";
+      premioN3.style.display = "none";
+      premioN4.style.display = "none";
+      break;
+//---------- PREMIO 1 - PUNTOS DE 50 A 79 -------
+    case 2:
+      sinPremiosN.style.display = "none";
+      premioN1.style.display = "block";
+      premioN2.style.display = "none";
+      premioN3.style.display = "none";
+      premioN4.style.display = "none";
+
+      break;
+//---------- PREMIO 2 - PUNTOS DE 80 A 109 -------
+    case 3:
+      sinPremiosN.style.display = "none";
+      premioN1.style.display = "block";
+      premioN2.style.display = "block";
+      premioN3.style.display = "none";
+      premioN4.style.display = "none";
+
+      break;
+//---------- PREMIO 3 - PUNTOS DE 110 A 129 -------
+    case 4:
+      sinPremiosN.style.display = "none";
+      premioN1.style.display = "block";
+      premioN2.style.display = "block";
+      premioN3.style.display = "block";
+      premioN4.style.display = "none";
+
+      break;
+//---------- PREMIO 4 - PUNTOS DE 130 A 150 -------
+    case 5:
+      sinPremiosN.style.display = "none";
+      premioN1.style.display = "block";
+      premioN2.style.display = "block";
+      premioN3.style.display = "block";
+      premioN4.style.display = "block";
+      break;
+    default:
+
+  }
+
+
 });
